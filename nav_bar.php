fc29304dd7f60ba2ac4e1e4f446e6e02df7ea773
Eddie Banz <niwdebanz@gmail.com>
2015-03-23 17:47:23 -0700

    adding additional source

---
+++ nav_bar.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Regular Nav Bar</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<style>
+	nav {
+		color: white;
+	}
+	li {
+		margin-right: 30px;
+	}
+
+	</style>
+</head>
+</html>
+
+<nav class="nav navbar-inverse" role="navigation" id="nav">
+	<div class="nav navbar-collapse collapse navbar-nav navbar-left"><h3>Dojo eCommerce</h3></div>
+		 <ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Shopping Cart (5)</a></li>
+		</ul>
+	</div>
+</nav>
